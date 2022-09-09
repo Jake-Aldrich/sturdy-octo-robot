9672b628a810bff536277188d6cf0a4fb62bbd32
Jake Aldrich <sendjakeanemail@gmail.com>
2022-09-08 22:04:54 -0400

    Fix fs imports

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const axios = require('axios');
-const fs = require('node:fs');
+const fs = require('fs');
 const { Client, Collection, Intents } = require('discord.js');
 require('dotenv').config()
 
